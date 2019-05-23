246cc7c76428d787a21f917d26c21631028ce4a6
WindAsMe <542636539@qq.com>
2019-05-23 22:12:15 +0800

    Deep Learning

---
+++ venv/include/knn.py
@@ -0,0 +1,16 @@
+# !/usr/bin/python3
+# -- coding: UTF-8 --
+# Author   :WindAsMe
+# Date     :19-5-23 下午9:56
+# File     :knn.py
+# Location:/Home/PycharmProjects/..
+from sklearn import neighbors
+from sklearn import datasets
+
+
+if __name__ == '__main__':
+    knn_classify = neighbors.KNeighborsClassifier()
+    iris = datasets.load_iris()
+    knn_classify.fit(iris.data, iris.target)
+    predict_label = knn_classify.predict([[0.1, 0.2, 0.3, 0.4]])
+    print(predict_label)
