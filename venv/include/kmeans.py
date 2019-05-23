6a8220698ac82413cd14ac013a36ea76599b22a9
WindAsMe <542636539@qq.com>
2019-05-23 13:42:46 +0800

    Deep Learning

---
+++ venv/include/kmeans.py
@@ -0,0 +1,36 @@
+# !/usr/bin/python3
+# -- coding: UTF-8 --
+# Author   :WindAsMe
+# Date     :19-5-23 下午12:05
+# File     :kmeans.py
+# Location:/Home/PycharmProjects/..
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_iris
+
+
+if __name__ == '__main__':
+    iris = load_iris()
+    X = iris.data[:]
+    # display the distribution
+    # plt.scatter(X[:, 0], X[:, 1], c="red", marker='o', label='see')
+    # plt.xlabel('petal length')
+    # plt.ylabel('petal width')
+    # plt.legend(loc=2)
+    # plt.show()
+
+    estimator = KMeans(n_clusters=3)
+    estimator.fit(X)
+    label_pred = estimator.labels_
+
+    x0 = X[label_pred == 0]
+    x1 = X[label_pred == 1]
+    x2 = X[label_pred == 2]
+    plt.scatter(x0[:, 0], x0[:, 1], c="red", marker='o', label='label0')
+    plt.scatter(x1[:, 0], x1[:, 1], c="green", marker='*', label='label1')
+    plt.scatter(x2[:, 0], x2[:, 1], c="blue", marker='+', label='label2')
+    plt.xlabel('petal length')
+    plt.ylabel('petal width')
+    plt.legend(loc=2)
+    plt.show()
