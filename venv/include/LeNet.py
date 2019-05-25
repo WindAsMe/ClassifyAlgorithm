f9b311caa74e417a86c6378820508507fa80783a
WindAsMe <542636539@qq.com>
2019-05-25 23:47:10 +0800

    Deep Learning

--- venv/include/LeNet.py
+++ venv/include/LeNet.py
@@ -6,9 +6,25 @@
 # Location:/Home/PycharmProjects/..
 from keras import layers
 from keras.models import Model
+import numpy as np
 
 
-def lenet_5(in_shape=(32,32,1), n_classes=10, opt='sgd'):
+class ImageLoader(Loader):
+    def get_picture(self, content, index):
+        # file head is 16 byte
+        # 28 * 28 is a picture
+        start = index * 28 * 28 + 16
+        picture = []
+        for i in range(28):
+            # add a element
+            picture.append([])
+            for j in range(28):
+                byte1 = content[start + i * 28 + j]
+                picture[i].append(byte1)
+        return picture
+
+
+def lenet_5(in_shape=(32, 32, 1), n_classes=10, opt='sgd'):
     in_layer = layers.Input(in_shape)
     conv1 = layers.Conv2D(filters=20, kernel_size=5, padding='same', activation='relu')(in_layer)
     pool1 = layers.MaxPool2D()(conv1)
@@ -23,5 +39,9 @@ def lenet_5(in_shape=(32,32,1), n_classes=10, opt='sgd'):
 
 
 if __name__ == '__main__':
-    model = lenet_5()
-    print(model.summary())
+    # model = lenet_5()
+    # image = ImageLoader('train-images.idx3-ubyte', 5)
+    image = ImageLoader('t10k-images.idx3-ubyte')
+    binfile = open(filename, 'rb')
+    print(binfile.read(10))
+    # print(model.summary())
