34a20be0e100d6c85e087d98c090544a9423c3d4
WindAsMe <542636539@qq.com>
2019-05-27 22:08:11 +0800

    Deep Learning

---
+++ venv/include/bayes.py
@@ -0,0 +1,24 @@
+# !/usr/bin/python3
+# -- coding: UTF-8 --
+# Author   :WindAsMe
+# Date     :19-5-27 下午9:55
+# File     :bayes.py
+# Location:/Home/PycharmProjects/..
+from sklearn import datasets
+# Gaussian Naive Bayes
+from sklearn.naive_bayes import GaussianNB
+# Multinomial Naive Bayes
+from sklearn.naive_bayes import MultinomialNB
+
+
+if __name__ == '__main__':
+    iris = datasets.load_iris()
+    gnb = GaussianNB()
+    gnb.fit(iris.data, iris.target)
+    y_pre = gnb.predict(iris.data)
+    print("Gaussian Naive Bayes\n sample: %d  wrong: %d" % (iris.data.shape[0], (iris.target != y_pre).sum()))
+
+    mnb = MultinomialNB()
+    mnb.fit(iris.data, iris.target)
+    y_pre = mnb.predict(iris.data)
+    print("Multinomial Naive Bayes\n sample: %d  wrong: %d" % (iris.data.shape[0], (iris.target != y_pre).sum()))
